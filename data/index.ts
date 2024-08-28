8342f1edd1d83e9f54c56e83468987e13c67def5
Naveen <103026548+TynzX@users.noreply.github.com>
2024-08-28 05:11:08 +0000

    footer updated and tab icon changed

--- data/index.ts
+++ data/index.ts
@@ -205,13 +205,15 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/TynzX",
   },
-  {
-    id: 2,
-    img: "/twit.svg",
-  },
+  // {
+  //   id: 2,
+  //   img: "/twit.svg",
+  // },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/naveendevelops/",
   },
 ];
